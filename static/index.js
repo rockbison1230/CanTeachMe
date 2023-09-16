bf6a9a46628f911211b0f0a85b6c3bae5f25354c
amana <107503777+rockbison1230@users.noreply.github.com>
2023-09-16 16:05:21 -0400

    modified landing page of main page and edited css

--- static/index.js
+++ static/index.js
@@ -1,5 +1,25 @@
-function sendFlask()
-{
-    document.getElementById("submit-button").submit();
+function submitText() {
+    const inputText = document.getElementById('expression').value;
+
+    fetch('/', {
+        method: 'POST',
+        body: JSON.stringify({ text: userInput }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    
+    .then(response => response.json())
+    .then(data => {
+        const resultsContainer = document.getElementById('sentiment');
+        resultsContainer.innerHTML += `<p>Sentiment: ${data.sentiment}</p>`;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+    // Clear the input field after submission
+    document.getElementById('textInput').value = '';
 }
+
     
