170dc22309698ada0a7066df5b4f1c7e7fbb06d3
amana <107503777+rockbison1230@users.noreply.github.com>
2023-09-16 19:12:28 -0400

    trying js

--- static/index.js
+++ static/index.js
@@ -22,4 +22,36 @@ function submitText() {
     document.getElementById('textInput').value = '';
 }
 
+        
+$(function() {
+    $('nav').each(function() {
+        var $active, $content, $links = $(this).find('a');
+
+        $active = $($links.filter('[href="' + location.hash + '"]')[0] || $links[0]);
+        $active.addClass('active');
+
+        $content = $($active[0].hash);
+
+        $links.not($active).each(function() {
+            $(this.hash).hide();
+        });
+
+        $(this).on('click', 'a', function(e) {
+            $active.removeClass('active');
+            $content.hide();
+
+            $active = $(this);
+            $content = $(this.hash);
+
+            $active.addClass('active');
+            $content.show();
+
+            e.preventDefault();
+        });
+    });
+});
+    
+    
+  
+
     
