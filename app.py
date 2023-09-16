5d235e4012c0f638cb6a1394825f4e267763d988
amana <107503777+rockbison1230@users.noreply.github.com>
2023-09-16 06:15:25 -0400

    changes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 import nltk
 from textblob import TextBlob
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import openai
 import os
 
@@ -11,6 +12,17 @@ analyzer = SentimentIntensityAnalyzer()
 
 openai.api_key = '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'
 # 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
+general_positive = [('how are you?', 'positive'),
+                    ('have you eaten?', 'positive'),
+                    ('how was your day?', 'positive'),
+                    ('how are you doing?', 'positive'),
+                    ('how are you feeling?', 'positive'),
+                    ('how are you', 'positive'),
+                    ('have you eaten', 'positive'),
+                    ('how was your day', 'positive'),
+                    ('how are you doing', 'positive'),
+                    ('how are you feeling', 'positive')
+                    ]
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -19,8 +31,7 @@ def index():
     sentiment = None
     if request.method == "POST":
         userInput = request.form.get("expression")
-        analysis = TextBlob(userInput)
-        sentiment_score = analysis.sentiment.polarity
+        sentiment_score = analyzer.polarity_scores(userInput)
         if sentiment_score > 0:
             sentiment = "Positive"
         elif sentiment_score < 0:
@@ -36,14 +47,15 @@ def index():
         if sentiment == "Positive":
             response = openai.Completion.create(
                 engine="text-davinci-002",
-                prompt=f"Provide a detailed explanation for why the sentiment of the input is positive.: '{userInput}'",
+                prompt=f"Provide a detailed explanation for why the sentiment of the input is positive. Example: '{userInput}'",
                 max_tokens=60
             )
-        if sentiment == "Negative":
+        explanation = response.choices[0].text.strip()
+        if sentiment == "Negative" or sentiment == "Neutral":
             response = openai.Completion.create(
                 engine="text-davinci-002",
-                prompt=f"Provide a detailed explanation for why the sentiment of the input is negative.: '{userInput}'",
-                max_tokens=60
+                prompt=f"Provide a detailed explanation for why the sentiment of the input is either neutral or negative.: '{userInput}'",
+                max_tokens=100
             )
         explanation = response.choices[0].text.strip()
         return render_template("index.html", userInput=userInput, sentiment=sentiment, suggestion=suggestion, explanation=explanation)
