bb8f72554f2688b0849e47769facc545c76d4611
amana <107503777+rockbison1230@users.noreply.github.com>
2023-09-16 02:38:07 -0400

    added changes to frontend

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, request, render_template
 import nltk
+from textblob import TextBlob
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import openai
 
 app = Flask(__name__)
+nltk.download('all')
 
-# openapi key: sk-5mEcjzLISMLrMR73wG9gT3BIbkFJLgtcYn
+analyzer = SentimentIntensityAnalyzer()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # first generate sentiment
+    sentiment = None
+    if request.method == "POST":
+        userInput = request.form.get("expression")
+        analysis = TextBlob(userInput)
+        sentiment_score = analysis.sentiment.polarity
+        if sentiment_score > 0:
+            sentiment = "Positive"
+        elif sentiment_score < 0:
+            sentiment = "Negative"
+        else:
+            sentiment = "Neutral"
+        return render_template("index.html", userInput=userInput, sentiment=sentiment)
+    else:
+        return render_template("index.html", userInput=None)
 
 
 if __name__ == '__main__':
