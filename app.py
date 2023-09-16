9db0b4392f3f92a1c18c44aa3b9c45891e8b270c
amana <107503777+rockbison1230@users.noreply.github.com>
2023-09-16 01:57:09 -0400

    added post method to form

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request, render_template
 import nltk
 import openai
 
 app = Flask(__name__)
 
 # openapi key: sk-5mEcjzLISMLrMR73wG9gT3BIbkFJLgtcYn
+
+
+@app.route('/', methods=["POST"])
+def index():
+    userExample = request.form("expression")
+    return render_template("index.html", userExample=userExample)
 
 
 if __name__ == '__main__':
