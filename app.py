0c308d3fabc4c09db3b9ce4604f928ef5fac68ff
amana <107503777+rockbison1230@users.noreply.github.com>
2023-09-17 08:12:40 -0400

    Update on all pages
    Update to navbar and footer

--- app.py
+++ app.py
@@ -10,17 +10,21 @@ analyzer = SentimentIntensityAnalyzer()
 
 openai.api_key = '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'
 # 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
-general_positive = [('how are you?', 'positive'),
-                    ('have you eaten?', 'positive'),
-                    ('how was your day?', 'positive'),
-                    ('how are you doing?', 'positive'),
-                    ('how are you feeling?', 'positive'),
-                    ('how are you', 'positive'),
-                    ('have you eaten', 'positive'),
-                    ('how was your day', 'positive'),
-                    ('how are you doing', 'positive'),
-                    ('how are you feeling', 'positive')
-                    ]
+general_phrases = [
+    {
+        "phrase": "I'd love it if you did this for me.",
+        "sentiment": "Positive",
+        "suggestion": "While it is phrased like a general statement expressing favor towards a hypothetical scenario, for many it is also a less direct, 'more polite,' way of saying 'Can you do this for me?'",
+        "explanation": "This input is said to have a positive sentiment due to the use of the word 'love' expressing strong positive feelings. It is then a precursor to the phrase, 'if you did this for me,' which means that someone doing a specific action would cause a positive feeling."
+    },
+    {
+        "phrase": "How are you?",
+        "sentiment": "Neutral",
+        "suggestion": "It helps to keep things simple and say a simple 'Great, and you?' if you'd rather not go in detail and let them continue the conversation, or share more about your feelings if you'd like. ",
+        "explanation": "This input is said to have a neutral sentiment. For many, 'how are you' is another way of being polite and starting a conversation. Especially if they are people you are not acquainted with, they are not often expecting a lengthy or even genuine reply. The phrase itself is open-ended and often has an expected reply of 'I'm good / okay.' "
+
+    },
+]
 
 
 @app.route('/index.html', methods=['GET', 'POST'])
@@ -29,6 +33,12 @@ def index():
     sentiment = None
     if request.method == "POST":
         userInput = request.form.get("expression")
+        for item in general_phrases:
+            if item["phrase"] in userInput:
+                sentiment = item["sentiment"]
+                suggestion = item["suggestion"]
+                explanation = item["explanation"]
+                return render_template("index.html", sentiment=sentiment, suggestion=suggestion, explanation=explanation)
         analysis = TextBlob(userInput)
         sentiment_score = analysis.sentiment.polarity
         if sentiment_score > 0:
